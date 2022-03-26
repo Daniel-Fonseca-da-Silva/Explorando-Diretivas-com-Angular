648fde08c9ec6ad5846195683a0b72be8624a1cc
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-26 18:37:48 -0300

    Iteração com ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,67 @@ export class AppComponent implements OnInit {
 
   count = 0
   randomNumber = 0
+  fruits = [
+    {
+      name: 'Banana',
+      price: 5.79
+    },
+    {
+      name: 'Peach',
+      price: 4.50
+    },
+    {
+      name: 'Apple',
+      price: 2.20
+    },
+    {
+      name: 'Cherry',
+      price: 0.59
+    },
+    {
+      name: 'Guava',
+      price: 2.45
+    },
+    {
+      name: 'Grape',
+      price: 3.79
+    }
+  ]
+
+  cars = [
+    {
+      name: 'Impala',
+      year: 1960
+    },
+    {
+      name: 'Honda Civic',
+      year: 1998
+    },
+    {
+      name: 'Subaru',
+      year: 2000
+    },
+    {
+      name: 'Ford Capri',
+      year: 1985
+    },
+    {
+      name: 'Cadillac DeVille',
+      year: 1968
+    },
+    {
+      name: 'Kaiser Carabela',
+      year: 1961
+    },
+    {
+      name: 'Ika Torino',
+      year: 1960
+    },
+    {
+      name: 'Dodge LeBaron',
+      year: 1965
+    }
+  ]
 
   constructor() {}
 
@@ -18,9 +79,8 @@ export class AppComponent implements OnInit {
       if(this.count === 15)
         clearInterval(interval)
     }, 1000)
-
     this.randomNumber = Math.floor(Math.random() * 11);
-
+    console.log(this.fruits)
   }
 
 }
